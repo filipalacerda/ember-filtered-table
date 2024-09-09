e8c02870f842ce1c88ff3e02057ed97a7f9c4cc3
Filipa Lacerda <lacerda.filipa@gmail.com>
2024-09-09 11:39:01 +0100

    Increment button

---
+++ app/components/increment.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class CounterComponent extends Component {
+  @tracked count = 0;
+
+  @action
+  increment() {
+    this.count = this.count + 1;
+  }
+}
